9051590aec49bdcebb39729f970cb8828cae65db
ZamiraShiguangoAndromedaJ <zamirashiguango@gmail.com>
2024-11-01 22:29:46 -0500

    puse nuevas variables

--- src/main/java/zamira/estudiante/prueba/Prueba.java
+++ src/main/java/zamira/estudiante/prueba/Prueba.java
@@ -11,6 +11,19 @@ package zamira.estudiante.prueba;
 public class Prueba {
 
     public static void main(String[] args) {
-        System.out.println("Hello hola!");
+        
+        System.out.println("hyola brigit");
+        System.out.println("BRIGIT SUMA DOS NUMEROS");
+        int num1=45;
+        int num2=5;
+        int resultado=num1+num2;
+        System.out.println("la respuesta es:"+resultado);
+        
+        String nombre="zamira";
+        String apellido="Shiguango";
+        System.out.println("bienvenido,"+nombre+" "+apellido);
+        
+        
+        
     }
 }
